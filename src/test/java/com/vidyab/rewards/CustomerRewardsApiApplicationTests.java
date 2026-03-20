c5a38a0cc9b6df4a36c8d0449606f745335dc18b
praveen-vidya-b <praveenvidya14@gmail.com>
2026-03-20 15:47:42 +0530

    initial commit

---
+++ src/test/java/com/vidyab/rewards/CustomerRewardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidyab.rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerRewardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
